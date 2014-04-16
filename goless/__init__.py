2d46c1311023a2c6b70f15cee7a0525c9e12bd84
Rob Galanakis <rob.galanakis@gmail.com>
2014-04-16 15:10:17 -0400

    Added "panic" support. Goroutines exit on error.

--- goless/__init__.py
+++ goless/__init__.py
@@ -1,4 +1,5 @@
-"""``goless`` introduces go-like channels and select to Python,
+"""
+``goless`` introduces go-like channels and select to Python,
 built on top of Stackless Python (and maybe one day gevent).
 Use :func:`goless.chan` to create a synchronous or buffered channel.
 Use :func:`goless.select` like you would the ``Select`` function in Go's reflect package
@@ -6,8 +7,10 @@ Use :func:`goless.select` like you would the ``Select`` function in Go's reflect
 wasn't very effective).
 """
 
+import logging
 import stackless as _stackless
-
+import sys
+import traceback
 
 from .channels import chan, ChannelClosed
 from .select import dcase, rcase, scase, select
@@ -17,6 +20,24 @@ version_info = 0, 0, 1
 version = '.'.join([str(v) for v in version_info])
 
 
+def on_panic(etype, value, tb):
+    """Called when there is an unhandled error in a goroutine.
+    Logs and exits the process."""
+    logging.critical(traceback.format_exception(etype, value, tb))
+    _stackless.getmain().throw(SystemExit, 1)
+
+
 def go(func):
-    """Run a function in a new tasklet, like a goroutine."""
-    _stackless.tasklet(func)()
+    """
+    Run a function in a new tasklet, like a goroutine.
+    If the goroutine raises an unhandled exception (*panics*),
+    the :func:`on_panic` will be called,
+    which by default logs the error and exits the process.
+    """
+    def safe_wrapped(f):
+        # noinspection PyBroadException
+        try:
+            f()
+        except:
+            on_panic(*sys.exc_info())
+    _stackless.tasklet(safe_wrapped)(func)
--- tests/test_goless.py
+++ tests/test_goless.py
@@ -1,3 +1,4 @@
+import stackless
 import unittest
 
 import goless
@@ -5,4 +6,31 @@ from goless.debug import debug
 
 
 class GoTests(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        # Make sure unhandled exceptions are observed in the context
+        # of a single test.
+        stackless.schedule()
+
+        oldpanic = goless.on_panic
+        self.panic_calls = []
+        goless.on_panic = lambda *a: self.panic_calls.append(a)
+
+        def restore_panic():
+            goless.on_panic = oldpanic
+        self.addCleanup(restore_panic)
+
+        self.addCleanup(stackless.schedule)
+
+    def test_starts_stuff(self):
+        items = []
+        goless.go(lambda: items.append(1))
+        stackless.schedule()
+        self.assertEqual(items, [1])
+
+    def test_exc(self):
+        def raiseit():
+            raise RuntimeError('ha!')
+        goless.go(raiseit)
+        stackless.schedule()
+        self.assertEqual(len(self.panic_calls), 1)

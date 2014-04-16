49f89fc9f64e50be18f90adf93b5de2746085973
Rob Galanakis <rob.galanakis@gmail.com>
2014-04-16 19:25:35 -0400

    Deleted debug.py and references.

--- goless/debug.py
+++
@@ -1,6 +0,0 @@
-DEBUG = False
-
-
-def debug(s, *args):
-    if DEBUG:
-        print s % args
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,7 +6,6 @@ import time
 import unittest
 
 import goless
-from goless.debug import debug
 
 
 class Examples(unittest.TestCase):
@@ -18,23 +17,18 @@ class Examples(unittest.TestCase):
 
         def func1():
             time.sleep(.1)
-            debug('sending1')
             c1.send('one')
-            debug('sent1')
         goless.go(func1)
 
         def func2():
             time.sleep(.2)
-            debug('sending2')
             c2.send('two')
-            debug('sent2')
         goless.go(func2)
 
         # We don't print since we run this as a test.
         callbacks = []
 
         for i in range(2):
-            debug('loop %s', i)
             _, val = goless.select([goless.rcase(c1), goless.rcase(c2)])
             callbacks.append(val)
 

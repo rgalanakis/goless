74d7360833d789da417023b050a5d1f00d1af5c2
Rob Galanakis <rob.galanakis@gmail.com>
2014-05-30 10:21:44 -0500

    Added tests for select.

--- benchmark.py
+++ benchmark.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 import time
 
-from goless import chan, go, backends
+from goless import backends, chan, go, selecting
 
 
 QUEUE_LEN = 10000
@@ -36,9 +36,42 @@ def bench_channels():
     print('Benchmarking channels:')
     for size, name in CHANSIZE_AND_NAMES:
         took = bench_channel(size)
-        print ('  %s: %s' % (took, name))
+        print ('  %ss: %s' % (took, name))
+
+
+def bench_select(use_default):
+    c = chan(0)
+    cases = [
+        selecting.scase(c, 1),
+        selecting.rcase(c),
+        selecting.scase(c, 1),
+        selecting.rcase(c),
+    ]
+    if use_default:
+        cases.append(selecting.dcase())
+
+    def sender():
+        while True:
+            c.send(0)
+            c.recv()
+    go(sender)
+
+    start = time.clock()
+    for i in xrange(QUEUE_LEN):
+        selecting.select(cases)
+    end = time.clock()
+    return end - start
+
+
+def bench_selects():
+    print('Benchmarking select:')
+    took_nodefault = bench_select(False)
+    print('  %ss: No default case.' % took_nodefault)
+    took_withdefault = bench_select(True)
+    print('  %ss: With default case.' % took_withdefault)
 
 
 if __name__ == '__main__':
     print('Using backend %s' % backends.current.__class__.__name__)
     bench_channels()
+    bench_selects()

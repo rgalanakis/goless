ea35d19fe039c8687b246ec987d05be40d14ca66
Rob Galanakis <rob.galanakis@gmail.com>
2014-06-12 17:27:44 -0500

    Use RTD theme if available.

--- doc/conf.py
+++ doc/conf.py
@@ -189,7 +189,14 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'golessdoc'
 
+try:
+    # noinspection PyPackageRequirements,PyUnresolvedReferences
+    import sphinx_rtd_theme
 
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+except ImportError:
+    pass
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {

91724beb7feb9091526397660b97ef0ae198be3d
Rob Galanakis <rob.galanakis@gmail.com>
2014-06-11 22:13:01 -0500

    Fixed up setup.py to prepare for release. Added MANIFEST.in to remove tests folder.

--- setup.py
+++ setup.py
@@ -2,28 +2,28 @@ from setuptools import setup
 
 setup(
     name='goless',
-    version='0.0.1',
+    version='0.0.4',
     author='Rob Galanakis',
     author_email='rob.galanakis@gmail.com',
-    description="goless puts Go's channels and select into Python.",
+    description="Provides a Go-like concurrent programming style in Python.",
     long_description="""goless provides Go-like channels and a select function
-    in Python, written on top of Stackless Python.
-    You can **send** to and **recv** from buffered or unbuffered **channels**,
-    and use a Go-like **select** function.
-    It also has goroutines for good measure.
+    in Python, written on top of gevent, PyPy, or Stackless Python.
+    Using goless, you can write Go-style concurrent programs in Python.
     """,
     license='Apache',
-    keywords='tasklet stackless go concurrent threading async',
+    keywords='tasklet stackless go concurrent '
+             'threading async gevent go golang',
     url='https://github.com/rgalanakis/goless',
+    packages=['goless'],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3',
     ],
     test_suite='tests',
 )

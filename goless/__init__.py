7c3520b072808dae6b167b0129254ac6fc8b0815
Rob Galanakis <rob.galanakis@gmail.com>
2014-07-25 08:33:52 -0500

    Bumped version to 0.7 for new release.

--- goless/__init__.py
+++ goless/__init__.py
@@ -22,7 +22,7 @@ from .channels import chan, ChannelClosed
 from .selecting import dcase, rcase, scase, select
 
 
-version_info = 0, 6, 0
+version_info = 0, 7, 0
 version = '.'.join([str(v) for v in version_info])
 
 __version__ = version
